51d80e0fcecde7734d06ad4d44f52507e20a03fd
Alejandro Domin <alejandrodirm52@gmail.com>
2022-10-03 19:33:17 +0200

    commit new "x"

--- js/myscript.js
+++ js/myscript.js
@@ -227,14 +227,30 @@ $("#btn1").addEventListener("click",function(){
     input.setAttribute("placeholder","E-mail");
     input.setAttribute("name","E-mail[]");
     $("#form").appendChild(input);
-    myAlert("add new email input");
+    myAlert("add new email");
 });
 function $(selector){
     return document.querySelector(selector);
 }
 function myAlert(msg){
+    //console.log($("body").children[1]);
     var div=document.createElement("div");
     div.classList.add("alert");
     div.innerHTML=msg;
-    $("body").insertBefore(div,$("body".firstChild))
+    var close=document.createElement("span");
+    close.style.float="right";
+    close.classList.add("close");
+    close.innerHTML="X";
+    div.appendChild(close);
+    $("body").insertBefore(div,$("body").firstChild);
+    bind_close();
+}
+function bind_close(){
+    let elements =document.querySelectorAll(".close");
+    for(var i=elements.length-1;i>=0;i--){
+        let el=elements[i];
+        el.addEventListener("click",function(){
+            this.parentNode.style.display="none";
+        });
+    }
 }
