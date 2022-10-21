f3497e27ffa415ebd2e731ff07fc9564a55f750f
Alejandro Domin <alejandrodirm52@gmail.com>
2022-10-21 20:02:44 +0200

    onblur

--- js/depuracion.js
+++ js/depuracion.js
@@ -11,3 +11,49 @@ setTimeout(()=>{
 if (window.confirm("Do you really want to leave?")) {
     window.open("exit.html", "Thanks for Visiting!");
   }
+
+
+/*
+wandow.open(http://www..gOOgle.es negnt=400, w1ath=00)
+5
+nuevaVentana.blur();
+Function cerrarNueva () {
+if (nuevaVentana)
+nuevaVentana.close();
+nuevaventana nulls
+S1en
+if (window.confirm("Do you really want to 1leave ?")) {
+indow. open("exit. html Thanks for Visiting!")
+et
+7
+elset
+sign = prompt ("Di algo no?");
+8
+console. log("Has dicho: "*sign);
+console. log(document.forms [o1
+</script
+</head>
+38 body onLoad-"inicializar ()">
+9
+<hl>Abrimos y cerramos ventanas</hl>
+Ktorm>
+p> (anput type=DUtton 10Credrventdnd va
+input type="button" id="cerrar-ventana" value="Cerrar Nueva Ventana" </p>
+/torm>
+</boOy?
+6
+/html>
+7
+
+
+*/
+
+
+
+
+
+
+
+
+
+
